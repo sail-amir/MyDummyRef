e331106e5d66cbf3f6cc90ae2cf9415ff5f27268
Amir Mohammad <71436956+sail-amir@users.noreply.github.com>
2025-08-06 16:50:16 -0400

    test2

---
+++ Utilities.py
@@ -0,0 +1,5 @@
+class Utils:
+    @staticmethod
+    def print_hi(x, y):
+        """Static method to duplicate input."""
+        print(x, y)
---
+++ test2.py
@@ -0,0 +1,23 @@
+class FirstClass:
+    def duplicate(self, x):
+        """Static method to duplicate input."""
+        return [x, x]
+    
+    def process(self, data):
+        """Uses Duplicator.duplicate() to process data."""
+        duplicated = self.duplicate(data)
+        return f"FirstClass processed: {duplicated}"
+    
+
+class SecondClass:
+    def duplicate(self, x):
+        """Static method to duplicate input."""
+        return [x, x]
+    
+    def display(self, item):
+        """Uses FirstClass.duplicate() in a different way."""
+        duplicated = self.duplicate(item)
+        return f"SecondClass displayed: {duplicated}"
+
+    def print_example(self):
+        print('hi')

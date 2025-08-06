a6eaf9c16b2d0512349298591a787d500f92a10f
Amir Mohammad <71436956+sail-amir@users.noreply.github.com>
2025-08-06 15:20:39 -0400

    initate move mehotd

--- examples5.py
+++ examples5.py
@@ -1,22 +1,16 @@
+from Utils import duplicate
+
 class FirstClass:
-    def duplicate(x):
-        """Static method to duplicate input."""
-        return [x, x]
-    
     def process(self, data):
         """Uses Duplicator.duplicate() to process data."""
-        duplicated = self.duplicate(data)
+        duplicated = duplicate(data)
         return f"FirstClass processed: {duplicated}"
     
 
 class SecondClass:
-    def duplicate(x):
-        """Static method to duplicate input."""
-        return [x, x]
-    
     def display(self, item):
         """Uses FirstClass.duplicate() in a different way."""
-        duplicated = self.duplicate(item)
+        duplicated = duplicate(item)
         return f"SecondClass displayed: {duplicated}"
 
     def print_example(self):

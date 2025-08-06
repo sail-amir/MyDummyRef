e89773c7dbb167f8f5bf8be0f61d569e4743f718
Amir Mohammad <71436956+sail-amir@users.noreply.github.com>
2025-08-06 16:18:24 -0400

    simulate move method static

--- Utils2.py
+++ Utils2.py
@@ -2,4 +2,9 @@ class Utils:
     @staticmethod
     def print_hi(x, y):
         """Static method to duplicate input."""
-        print(x, y)  
+        print(x, y)
+    
+    @staticmethod
+    def duplicate(x):
+        """Static method to duplicate input."""
+        return [x, x]
--- example9.py
+++ example9.py
@@ -1,11 +1,8 @@
+import Utils2
 class SecondClass:
-    def duplicate(x):
-        """Static method to duplicate input."""
-        return [x, x]
-    
     def display(self, item):
         """Uses FirstClass.duplicate() in a different way."""
-        duplicated = self.duplicate(item)
+        duplicated = Utils2.duplicate(item)
         return f"SecondClass displayed: {duplicated}"
 
     def print_example(self):

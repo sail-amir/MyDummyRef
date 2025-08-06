282405645c4796e50ab8a528796912aae6240288
Amir Mohammad <71436956+sail-amir@users.noreply.github.com>
2025-08-06 17:11:50 -0400

    test4

---
+++ test4.py
@@ -0,0 +1,17 @@
+from Utilities import Utils
+
+class FirstClass:  
+    def process(self, data):
+        """Uses Duplicator.duplicate() to process data."""
+        duplicated = Utils.duplicate(data)
+        return f"FirstClass processed: {duplicated}"
+    
+class SecondClass:
+    def display(self, item):
+        """Uses FirstClass.duplicate() in a different way."""
+        duplicated = Utils.duplicate(item)
+        self.print_example()
+        return f"SecondClass displayed: {duplicated}"
+
+    def print_example(self):
+        print('hi')

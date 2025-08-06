8ddd3fdf71e78353ebd5bfd0159c83df1b119190
Amir Mohammad <71436956+sail-amir@users.noreply.github.com>
2025-08-06 14:49:34 -0400

    refactor

--- clone.py
+++ clone.py
@@ -1,19 +1,16 @@
-class FirstClass:
+class ZeroClass:
     def duplicate(self, x):
         """Creates a duplicate (copy) of the input."""
         return [x, x]  # Same implementation in both classes
-    
+
+class FirstClass(ZeroClass):
     def process(self, data):
         """Uses duplicate() to process data."""
         duplicated = self.duplicate(data)
         return f"FirstClass processed: {duplicated}"
 
 
-class SecondClass:
-    def duplicate(self, x):
-        """Same implementation as in FirstClass."""
-        return [x, x]  # Exact clone of FirstClass.duplicate()
-    
+class SecondClass(ZeroClass):
     def display(self, item):
         """Uses duplicate() in a different way."""
         duplicated = self.duplicate(item)

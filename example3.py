c4a99984c52de75a3cf379ea3e48d226c6553053
Amir Mohammad <71436956+sail-amir@users.noreply.github.com>
2025-08-06 15:01:32 -0400

    refactor

--- example3.py
+++ example3.py
@@ -8,10 +8,12 @@ class FirstClass:
         duplicated = Duplicator.duplicate(data)
         return f"FirstClass processed: {duplicated}"
     
-    def print_example(self):
-        print('hi')
+
 class SecondClass:
     def display(self, item):
         """Uses FirstClass.duplicate() in a different way."""
         duplicated = FirstClass.duplicate(item)
         return f"SecondClass displayed: {duplicated}"
+
+    def print_example(self):
+        print('hi')

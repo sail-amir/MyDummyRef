526b185a546586b3dd5aa82b764be8342ea3a698
Amir Mohammad <71436956+sail-amir@users.noreply.github.com>
2025-08-06 16:10:05 -0400

    revert

--- example7.py
+++ example7.py
@@ -1,9 +1,11 @@
-from Utils import duplicate
-
 class SecondClass:
+    def duplicate(x):
+        """Static method to duplicate input."""
+        return [x, x]
+    
     def display(self, item):
         """Uses FirstClass.duplicate() in a different way."""
-        duplicated = duplicate(item)
+        duplicated = self.duplicate(item)
         return f"SecondClass displayed: {duplicated}"
 
     def print_example(self):
